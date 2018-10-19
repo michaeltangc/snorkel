ddc44149d80ac25f776afb0ebed26dd74f71846c
Ines Chami <ineschami@hotmail.com>
2018-10-19 12:19:11 -0700

    fixed bug with test set

--- tutorials/cdr/train_models_parallel.py
+++ tutorials/cdr/train_models_parallel.py
@@ -7,10 +7,10 @@ SLEEP = 10
 CTR_LIM = 12
 procs_lim = 12
 l_vals = range(33)
-d_vals = np.arange(10, 250, 10)
+d_vals = np.arange(10, 250, 5)
 script = 'train_model.py'
 datapath = 'cdr_data/'
-n_runs = 3  # (for labelling functions)
+n_runs = 3
 save_file = 'cdr_training_{}.log'
 
 
@@ -26,31 +26,31 @@ if __name__ == '__main__':
     j = 0
     # loop over d
     for d in d_vals:
-        j += 1
-        proc_args = [
-            'python', script,
-            '--hidden_dim', str(d),
-            '--lfs_indices', ','.join([str(x) for x in l_vals]),
-            '--datapath', datapath,
-            '--save_file', save_file.format('d')
-        ]
-        print("Launching model {0}".format(j))
-        p = subprocess.Popen(proc_args)
-        procs.append(p)
-        ctr = 0
-        while True:
-            k = num_procs_open(procs)
-            ctr += 1
-            if ctr >= CTR_LIM:
-                ctr = 0
-                print('{0} processes still running'.format(k))
-            if num_procs_open(procs) >= procs_lim:
-                time.sleep(SLEEP)
-            else:
-                break
+        for _ in range(n_runs):
+            j += 1
+            proc_args = [
+                'python', script,
+                '--hidden_dim', str(d),
+                '--lfs_indices', ','.join([str(x) for x in l_vals]),
+                '--datapath', datapath,
+                '--save_file', save_file.format('d')
+            ]
+            print("Launching model {0}".format(j))
+            p = subprocess.Popen(proc_args)
+            procs.append(p)
+            ctr = 0
+            while True:
+                k = num_procs_open(procs)
+                ctr += 1
+                if ctr >= CTR_LIM:
+                    ctr = 0
+                    print('{0} processes still running'.format(k))
+                if num_procs_open(procs) >= procs_lim:
+                    time.sleep(SLEEP)
+                else:
+                    break
     # loop over l
     for l in l_vals:
-        break
         for _ in range(n_runs):
             l_indices = np.random.choice(l_vals, l+1, replace=False)
             j += 1
